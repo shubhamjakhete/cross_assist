91a2eeaf905d68c0d7c78573d818d7c9ba1e9518
Shubham Jakhete <shubhamjakhete1997@gmail.com>
2026-03-07 19:37:23 -0800

    Initial Commit

---
+++ CrossAssist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrossAssist
+//
+//  Created by Shubham Jakhete on 3/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrossAssist/CrossAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossAssistApp.swift
+//  CrossAssist
+//
+//  Created by Shubham Jakhete on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct CrossAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
